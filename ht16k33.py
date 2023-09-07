3d7830362b077d9241297946444d7b44ccb14754
Andrew <andrewtudoroi@gmail.com>
2023-09-07 23:41:21 +0300

    Fix wiring mistacke, implent trivial clock

--- ht16k33.py
+++ ht16k33.py
@@ -1,6 +1,7 @@
 from smbus2 import SMBus
 import time
 from font5x7 import Font5x7
+from datetime import datetime
 
 HT16K33_ADDRESS_1 = 0x71
 HT16K33_ADDRESS_0 = 0x70
@@ -8,8 +9,9 @@ HT16K33_CMD_BRIGHTNESS = 0xE0
 
 
 class HT16K33():
+
     def __init__(self, ht16k33_i2c_address):
-        
+
         bus = SMBus(0)
         #turn on oscilator 
         bus.write_byte(ht16k33_i2c_address, 0x21)
@@ -18,7 +20,7 @@ class HT16K33():
         #clear dispay 
         bus.write_i2c_block_data(ht16k33_i2c_address, 0x00, [0x00] * 16)
         #set brightness 0-15
-        bus.write_byte(ht16k33_i2c_address, HT16K33_CMD_BRIGHTNESS | 10)
+        bus.write_byte(ht16k33_i2c_address, HT16K33_CMD_BRIGHTNESS | 5)
         self.ht16k33_i2c_address = ht16k33_i2c_address
         self.bus = bus
         self.buffer = [0x00 for x in range(0, 16)]
@@ -107,13 +109,21 @@ ht_1.clear()
 ht_0 = HT16K33(ht16k33_i2c_address=HT16K33_ADDRESS_0)
 ht_0.clear()
 
+
 while True:
     for i in range(0 , 10 -3):
+        current_time = datetime.now().strftime("%H%M%S")
         #bus.write_byte(HT16K33_ADDRESS, HT16K33_CMD_BRIGHTNESS | i)
-        ht_1.write_number(Font5x7[i], Font5x7[i+1], Font5x7[i+2])
-        ht_0.write_number(Font5x7[i], Font5x7[i], Font5x7[i])
-        time.sleep(0.5)
+        ht_1.write_number(Font5x7[int(current_time[5])], Font5x7[int(current_time[4])], Font5x7[int(current_time[3])])
+
+        #Workaraund for mistake in shematic connection on ds2 
+        ch = [x for x in Font5x7[int(current_time[1])]] #Create separate array 
+        j = ch[3]
+        ch[3] = ch[4]
+        ch[4] = j
+        ht_0.write_number(Font5x7[int(current_time[2])], ch, Font5x7[int(current_time[0])])
+
+        time.sleep(1)
         ht_1.clear()
         ht_0.clear()
-
 
