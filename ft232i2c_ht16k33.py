ed498972c33cfee72f1302deb84c5a0123dda7bc
Andrew <andrewtudoroi@gmail.com>
2023-10-11 20:44:06 +0300

    Add ft232h example via i2c

--- ft232i2c_ht16k33.py
+++ ft232i2c_ht16k33.py
@@ -1,4 +1,4 @@
-from smbus import SMBus
+import board
 import time
 from font5x7 import Font5x7_full as Font5x7
 from datetime import datetime
@@ -9,19 +9,35 @@ HT16K33_ADDRESS_1 = 0x71
 HT16K33_ADDRESS_0 = 0x70
 HT16K33_CMD_BRIGHTNESS = 0xE0
 
+# bus = board.I2C()
+
+# ht16k33_i2c_address = 0x70
+# #turn on oscilator 
+# bus.writeto(ht16k33_i2c_address, [0x21])
+# #enable display (no blinking mode)
+# bus.writeto(ht16k33_i2c_address, [0x81])
+# #clear dispay 
+# bus.writeto(ht16k33_i2c_address, [0x00] + [0x00] * 16)
+# #set brightness 0-15
+# bus.writeto(ht16k33_i2c_address, [HT16K33_CMD_BRIGHTNESS | 15] )
+#         #grafic buffer 
+# buffer = [0xff for x in range(0, 16)]
+# bus.writeto(ht16k33_i2c_address, [0x00] + buffer)
+
+
 
 class HT16K33():
 
     def __init__(self, ht16k33_i2c_address):
-        bus = SMBus(2)
+        bus = board.I2C()
         #turn on oscilator 
-        bus.write_byte(ht16k33_i2c_address, 0x21)
+        bus.writeto(ht16k33_i2c_address, [0x21] )
         #enable display (no blinking mode)
-        bus.write_byte(ht16k33_i2c_address, 0x81)
+        bus.writeto(ht16k33_i2c_address, [0x81] )
         #clear dispay 
-        bus.write_i2c_block_data(ht16k33_i2c_address, 0x00, [0x00] * 16)
+        bus.writeto(ht16k33_i2c_address, [0x00] + [0x00] * 16)
         #set brightness 0-15
-        bus.write_byte(ht16k33_i2c_address, HT16K33_CMD_BRIGHTNESS | 10)
+        bus.writeto(ht16k33_i2c_address, [HT16K33_CMD_BRIGHTNESS | 15])
         self.ht16k33_i2c_address = ht16k33_i2c_address
         self.bus = bus
         #grafic buffer 
@@ -31,8 +47,7 @@ class HT16K33():
 
     def rotate_90(self, a):
         b = []
-        for k in range(0, 7):
-            #q = [(a[0] & (1 << k)) >> k , (a[1] & (1 << k)) >> k , (a[2] & (1 << k)) >> k,  (a[3] & (1 << k)) >> k,  (a[4] & (1 << k)) >> k]	
+        for k in range(0, 7):           
             q = [(x & (1 << k)) >> k for x in a ]
             qi = 0
             for bit in q:    
@@ -42,12 +57,17 @@ class HT16K33():
 
     def clear(self):
         self.buffer = [ 0x00 for x in range(0, 16)]
-        #self.bus.write_i2c_block_data(self.ht16k33_i2c_address, 0x00, self.buffer)
+ 
 
     def read_key_data(self):
         #read all keys
         # key_data = self.bus.read_i2c_block_data(self.ht16k33_i2c_address, 0x40, 6)
-        key_data = self.bus.read_i2c_block_data(self.ht16k33_i2c_address, 0x45, 1)
+        # key_data = self.bus.read_i2c_block_data(self.ht16k33_i2c_address, 0x45, 1)
+        key_buffer = bytearray(1)
+        self.bus.readfrom_into(self.ht16k33_i2c_address, key_buffer,  start=0x45, end=0x46)
+        #  does not work  from 3.3 
+        print('keys', key_buffer)
+        key_data = [0]
         return key_data[0]
 
 
@@ -61,7 +81,7 @@ class HT16K33():
 
 
     def update(self):
-        self.bus.write_i2c_block_data(self.ht16k33_i2c_address, 0x00, self.buffer)
+        self.bus.writeto(self.ht16k33_i2c_address, [0x00] + self.buffer)
         
 
     def write_data_raw(self, a, b, c):  
@@ -123,7 +143,7 @@ class HT16K33():
         self.buffer[15] = 0x00
 
         #write data 
-        self.bus.write_i2c_block_data(self.ht16k33_i2c_address, 0x00, self.buffer)
+        self.bus.writeto(self.ht16k33_i2c_address, [0x00] + self.buffer)
 
     def write_data(self, a, b, c, show_decimal_point=False):  
         bx = []
@@ -198,11 +218,8 @@ class HT16K33():
             self.buffer[13] = self.buffer[13] | self.decimal_dot_bit << 7
 
         #write data 
-        self.bus.write_i2c_block_data(self.ht16k33_i2c_address, 0x00, self.buffer)
+        self.bus.writeto(self.ht16k33_i2c_address, [0x00] + self.buffer)
 
-
-
-#Initialise drivers 
 ht_1 = HT16K33(ht16k33_i2c_address=HT16K33_ADDRESS_1)
 ht_1.clear()
 
@@ -210,18 +227,6 @@ ht_0 = HT16K33(ht16k33_i2c_address=HT16K33_ADDRESS_0)
 ht_0.clear()
 
 
-# ht_1.decimal_dot()
-# ht_1.update()
-# time.sleep(1.2)
-# ht_1.clear()
-# ht_1.update()
-# time.sleep(1.2)
-# ht_1.decimal_dot()
-# ht_1.update()
-# time.sleep(6)
-
-
-#display string 6 char
 def display_print(font, str_data, show_decimal_point=False):
     #Clear buffer 
     ht_1.clear()
@@ -296,17 +301,13 @@ display_string = "Motanas si Pisicuta si Catelus) "
 pikachu_d = pikachu
 show_time = True
 display_menu = 0
-
-
 keys = 0b0000
+
 while True:
-    
     current_time = datetime.now().strftime("%H%M%S")
     # display_string = display_string 
     display_string = display_string[1:] + display_string[:1]
 
-
-    # 
     value = 1 if ht_1.read_key_data() == 16 else 0 
     #shift bits to the left 
     keys = keys << 1
@@ -323,29 +324,12 @@ while True:
          if display_menu == 3:
              display_menu = 0 
 
-
-    #--------Buttons reading----------------
-    # using python list for storage
-    # key_trace.append(ht_1.read_key_data())
-    # key_trace = key_trace[-2:]
-
-    # # Togle state only if previosly buton state was 0
-    # # Mening botton was realeased 
-    # if key_trace[0] == 0 and key_trace[1] == 16:
-    #     display_menu = display_menu + 1
-    #     if display_menu == 3:
-    #         display_menu = 0 
-
-    
-    
+        
     if display_menu == 0:
         display_print(Font5x7, current_time[:6], show_decimal_point=True)
-    #display update rate
-    # display_print(Font5x7, current_time)
         time.sleep(0.165)
  
     if display_menu == 1:
-        # display_print(Font5x7, display_string[:6])
         pikachu_d = pikachu_d[1:] + pikachu_d[:1]
         dispay_bitmap(pikachu_d)
         time.sleep(0.12)
@@ -354,4 +338,3 @@ while True:
         display_print(Font5x7, display_string[:6], show_decimal_point=False)
         time.sleep(0.12)
  
-
