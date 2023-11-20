0f1ca709e55aad4d514672a094ecbd5584aeb047
Andrew <andrewtudoroi@gmail.com>
2023-11-20 15:01:39 +0200

    Update timing

--- saa1064t.py
+++ saa1064t.py
@@ -66,7 +66,7 @@ while True:
         digit_1 = digit_font.get(key, 0x00)
         bus.write_i2c_block_data(SAA1064_ADDRESS, 0x01, [digit_1, 0x00, 0x00, 0x00])
         time.sleep(0.5)
-    time.sleep(2)
+    # time.sleep(0.5)
     # for key in 'PLAY ':
     #     digit_1 = digit_font.get(key, 0x00)
     #     bus.write_i2c_block_data(SAA1064_ADDRESS, 0x01, [digit_1, 0x00, 0x00, 0x00])
